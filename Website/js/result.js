a6d77cfb049f40178d38fd1eb03b06a57ca800b1
PikaTer <PikaTer@users.noreply.github.com>
2019-09-06 20:48:40 +0800

    Basic Functionality Done
    Most of the basic functions are done
    There are still minor part need to clean up, mainly in the result.js file
    Currently google's map geolocation API limits to few querys per x?x
    Finding a workaround...

--- Website/js/result.js
+++ Website/js/result.js
@@ -3,43 +3,53 @@ $(function()
     var serious = false;
     var distress = false;
 
+    var session_result = sessionStorage.getItem("result");
+    session_result = JSON.parse(session_result);
+
     // Force scroll to top
     $(document).scrollTop(0);
 
     // Disable dragable element
     $("section").css({"pointer-events": "none"});
 
-    // Dynamic variable to be read from session storage
-    var test_type = "Kesepian";
+    var test_type;
+    var test_count = 1;
     
-    // Dynamic title of test taken
-    $(".result_title").append("Keputusan Ujian " + test_type + " Anda");
-
-    var test_count = 3;
-
-    for(var i = 0; i < test_count; i++)
+    if(session_result["test"].length == 3)
     {
-        var test_type = "Kesepian";
-        var score = 8;
-        var full_score = 40;
+      test_type = "DASS";
+      $(".result_title").append("Keputusan Ujian " + test_type + " Anda");
 
-        var result_card = '<div class="col-md-4 text-center ftco-animate fadeInUp ftco-animated result-display">' +
+      for (var i = 0; i < 3; i++)
+        display_result(to_malay(session_result["test"][i]), session_result["score"][i], session_result["overallscore"]);
+    }
+    else
+    {
+      test_type = to_malay(session_result["test"]);
+      $(".result_title").append("Keputusan Ujian " + test_type + " Anda");
+
+      display_result(to_malay(session_result["test"]), session_result["score"], session_result["overallscore"]);
+    }
+
+    function display_result(test_type, score, full_score)
+    {
+      var result_card = '<div class="col-md-4 text-center ftco-animate fadeInUp ftco-animated result-display">' +
                         '<div class="steps">' +
                         '<div class="icon mb-4 d-flex justify-content-center align-items-center">' +
                         '<span class="flaticon-cloud-computing-1"></span>' +
                         '</div>' +
                         '<h3>Skor ' + test_type + '</h3>' +
                         '<p><strong class="number" data-number=' + score + '>0</strong><strong>/' + full_score + '</strong><br>' +
-                        result_text(test_type, score) +
+                        result_text(session_result["test"][i], score) +
                         '</div>' +
                         '</div>';
 
-        $(result_card).appendTo(".result-card");
+      $(result_card).appendTo(".result-card");
     }
 
-    if (distress)
+    if(distress)
     {
-      $(".result-card").append('<a href="#" class="btn btn-primary d-block px-3 py-3 mb-4 distress">Butang Kecemasan</a>');
+      $(".distress-button").append('<a href="#" class="btn btn-primary d-block px-3 py-3 mb-4 distress" style="display: none !important">Butang Kecemasan</a>');
       $(".distress-text").append('Memerlukan bantaun sekarang? Tekanlah butang kecemasan');
     }
 
@@ -66,7 +76,36 @@ $(function()
       $(".result-suggestion").append(table_header);
     }
     else
-      $(".help-text").append("Skor " + test_type + " anda adalah normal, anda mempunyai status mental yang sihat. Tahniah!")
+    {
+      $(".help-text").append("Skor " + test_type + " anda adalah normal, anda mempunyai status mental yang sihat. Tahniah!");
+      $(".help-text").append("<br><br><p class='news-text'>Di bawah merupakan beberapa berita atau artikel tentang " + test_type + ". Anda boleh mengetahui lebih dengan melawat link tersebut</p>");
+
+      var url = 'https://newsapi.org/v2/everything?' +
+                'qInTitle=' + to_english(session_result["test"]) + '&' +
+                'sortBy=popularity&' +
+                'apiKey=dc5c322ae4684614872e8dd7b4d1ece9';
+
+      var req = new Request(url);
+
+      fetch(req)
+        .then(response => response.json())
+        .then(function(newsObject) {
+          for(var i = 0; i < 6; i++)
+          {
+            var news_suggestion = '<div class="col-md-4 ftco-animate fadeInUp ftco-animated">' +
+                                  '<div class="blog-entry">' +
+                                  '<a href="' + newsObject["articles"][i]["url"] + '" class="block-20" style="background-image: url(\'' + newsObject["articles"][i]["urlToImage"] + '\');">' +
+                                  '</a>' +
+                                  '<div class="text d-flex py-4">' +
+                                  '<h3><a class="news-heading" href="' + newsObject["articles"][i]["url"] + '">' + newsObject["articles"][i]["title"] + '</a></h3>' +
+                                  '</div>' +
+                                  '</div>' +
+                                  '</div>'
+
+              $(".news-suggestion").append(news_suggestion);
+          }
+        });
+    }
 
     // custom section counter
     var counter = function() {
@@ -87,6 +126,8 @@ $(function()
 					  }, 4000
           );
           $('.scoring-status').delay(4000).fadeIn(1000);
+          $('.distress').delay(4000).fadeIn(1000);
+          $('.distress-text').delay(4000).fadeIn(1000);
 				});
 			}
 
@@ -119,14 +160,40 @@ $(function()
     });
   }
 
-  
+  function to_english(category)
+  {
+    if(category == 1)
+      return "Loneliness";
+    else if (category == 2)
+      return "Depression";
+    else if (category == 3)
+      return "Anxiety";
+    else if (category == 4)
+      return "Stress";
+    else if (category == 5)
+      return "Suicidal Thoughts";
+  }
+
+  function to_malay(category)
+  {
+    if(category == 1)
+      return "Kesepian";
+    else if (category == 2)
+      return "Kemurungan";
+    else if (category == 3)
+      return "Kerisauan";
+    else if (category == 4)
+      return "Stres";
+    else if (category == 5)
+      return "Ideasi Bunuh Diri";
+  }
   
   function result_text(test_type, score)
   {
     var accessment;
     var scale;
 
-    if(test_type == "Kemurungan")
+    if(test_type == 2)
     {
       if(score > 27)
       {
@@ -154,7 +221,7 @@ $(function()
         scale = 0;
       }
     }
-    else if(test_type == "Kerisauan")
+    else if(test_type == 3)
     {
       if(score > 19)
       {
@@ -182,7 +249,7 @@ $(function()
         scale = 0;
       }
     }
-    else if(test_type == "Stres")
+    else if(test_type == 4)
     {
       if(score > 33)
       {
@@ -210,7 +277,7 @@ $(function()
         scale = 0;
       }
     }
-    else if(test_type == "Kesepian")
+    else if(test_type == 1)
     {
       if(score > 29)
       {
@@ -235,12 +302,12 @@ $(function()
     }
     else
     {
-      if(score < 20)
+      if(score > 20)
       {
         accessment = "Teruk";
         scale = 4;
       }
-      else if (score < 8)
+      else if (score > 8)
       {
         accessment = "Sederhana";
         scale = 2;
@@ -293,7 +360,7 @@ function get_state()
     alert(err);
   }
 
-    var geocoder;
+    /*var geocoder;
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(latitude, longitude);
 
@@ -311,7 +378,7 @@ function get_state()
                     city=value[count-3];
                 }
         }
-    });
+    });*/
 
     return state;
 }
